a95839205634c086c5217dc386d6c2abccf86bad
Benjamin O'Brien <benjaminob6@gmail.com>
2012-11-12 12:36:31 -0500

    Calling the fancy new code

--- ArduBalance/ArduBalance.ino
+++ ArduBalance/ArduBalance.ino
@@ -5,9 +5,13 @@ Ben O'Brien - 12-Nov-2012
 *****************************/
 #include "Ardudefine.h"
 #include "IRSensor.h"
+#include "MotorDriver.h"
 
 IRSensor ForwardSensor(A0);
 IRSensor BackwardSensor(A1);
+
+//MotorDriver(int pwm_a, int a1, int a2, int pwm_b, int b1, int b2)
+MotorDriver Motor(10,3,2,11,4,5);
 
 void setup(){
   Serial.begin(115200);
@@ -21,4 +25,8 @@ void setup(){
 void loop(){
   //ForwardSensor.takeReading();
   //Serial.println(ForwardSensor.getLastReading());
+  //for ( int i = -255; i < 255; i++){
+  //  Motor.SetMotors(i,i);
+  //  delay(50);
+  //}
 }
